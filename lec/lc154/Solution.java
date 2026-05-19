4539f252516476483cdd8c0c2eb7f07b25a83776
sheeta1998 <2169298883@qq.com>
2026-05-19 20:11:20 +0800

    5.16

--- lec/lc154/Solution.java
+++ lec/lc154/Solution.java
@@ -1,19 +1,20 @@
-package lc153;
-
+package lc154;
 class Solution {
     int binary(int[] arr, int left, int right) {
         while (left != right) {
             int mid = left + (right - left) / 2;
             if (arr[mid] > arr[right]) {
                 left = mid + 1;
-            } else {
+            } else if (arr[mid] < arr[right]) {
                 right = mid;
+            } else {
+                right--;
             }
         }
         return arr[left];
     }
 
     public int findMin(int[] nums) {
-        return binary(nums, 0, nums.length-1);
+        return binary(nums, 0, nums.length - 1);
     }
 }
