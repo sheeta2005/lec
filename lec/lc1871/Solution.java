39a7c06cd157263afdb5c30df7c3c822afb47134
sheeta1998 <2169298883@qq.com>
2026-05-25 20:04:56 +0800

    5.25

---
+++ lec/lc1871/Solution.java
@@ -0,0 +1,31 @@
+package lc1871;
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        boolean[] dp = new boolean[n];
+        dp[0] = true;
+        int[] pre = new int[n + 1];
+        pre[1] = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            int L = Math.max(0, i - maxJump);
+            int R = i - minJump;
+
+            if (R < 0) {
+                dp[i] = false;
+            } else {
+                if (pre[R + 1] - pre[L] > 0 && s.charAt(i) == '0') {
+                    dp[i] = true;
+                } else {
+                    dp[i] = false;
+                }
+            }
+            pre[i + 1] = pre[i] + (dp[i] ? 1 : 0);
+        }
+        return dp[n - 1];
+    }
+}
