33de059cdf7ff48ba88a72528a76eebd6c5d70b6
sheeta1998 <2169298883@qq.com>
2026-05-27 09:21:39 +0800

    5.27

---
+++ lec/lc61/Solution.java
@@ -0,0 +1,58 @@
+package lc61;
+
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+        int n = 0;
+        ListNode count = head;
+        while (count.next != null) {
+            count = count.next;
+            n++;
+        }
+        n++;
+        if (n == 1) {
+            return head;
+        }
+        k = k % n;
+        if (k == 0) {
+            return head;
+        }
+        int headNum = n - k;
+        int tailNum = n - k - 1;
+        ListNode tail;
+        ListNode oldHead = head;
+
+        for (int i = 0; i < tailNum; i++) {
+            head = head.next;
+        }
+        tail = head;
+        head = tail.next;
+        tail.next = null;
+        ListNode last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = oldHead;
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
