69973a10fa802a1098738efc31e431fceb9e878e
sheeta1998 <2169298883@qq.com>
2026-06-16 10:03:17 +0800

    6.16

---
+++ lec/lc3600_lc3699/lc3612/Solution.java
@@ -0,0 +1,21 @@
+package lc3600_lc3699.lc3612;
+
+public class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == '*') {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } else if (c == '#') {
+                sb.append(sb);
+            } else if (c == '%') {
+                sb.reverse();
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
